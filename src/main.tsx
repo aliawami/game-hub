a1f13762c72ecd9ca1f3fca01197d067f9f99539
Ali Alawami <alis2012@gmail.com>
2024-12-24 14:42:43 +0300

    Added dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  defaultSystem,
+ 
+} from "@chakra-ui/react";
+import { ColorModeProvider } from "./components/ui/color-mode.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <App />
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </StrictMode>
 );
