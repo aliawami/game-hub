cec4cd86649fe5443cf2e1250db9359c35ad3c67
Ali Alawami <alis2012@gmail.com>
2024-12-29 10:50:41 +0300

    Clean up Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,11 @@
-import { HStack, Image, List, Spinner, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  Spinner,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/imageUrl";
 
@@ -12,27 +19,35 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (error) return null;
   if (isLoading) return <Spinner size={"lg"} />;
   return (
-    <List.Root listStyle={"none"}>
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY={"5px"}>
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize={"32px"}
-              borderRadius={8}
-            />
-            <Button
-              fontSize={"lg"}
-              variant={"plain"}
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
-            >
-              {genre.name}{" "}
-            </Button>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List.Root listStyle={"none"}>
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize={"32px"}
+                borderRadius={8}
+                objectFit={"cover"}
+              />
+              <Button
+                fontSize={"lg"}
+                variant={"plain"}
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+              >
+                {genre.name}{" "}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
